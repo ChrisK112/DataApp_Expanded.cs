2e101a4b7f485a3de6aa63c56e10a90f405b7ea8
ChrisK112 <krzysztofkolodynski06@gmail.com>
2023-10-25 21:52:37 +0100

    Initial Push with changes. -Added Profiles functionalitity

---
+++ TbManagementTool/Form2.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DataApp
+{
+    partial class Form2
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.radio_OverwriteCurrentProfile = new System.Windows.Forms.RadioButton();
+            this.radio_SaveAsNewProfile = new System.Windows.Forms.RadioButton();
+            this.textBox_SaveAsNewProfile = new System.Windows.Forms.TextBox();
+            this.button_SaveProfile = new System.Windows.Forms.Button();
+            this.button_CancelProfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radio_OverwriteCurrentProfile
+            // 
+            this.radio_OverwriteCurrentProfile.AutoSize = true;
+            this.radio_OverwriteCurrentProfile.Location = new System.Drawing.Point(26, 27);
+            this.radio_OverwriteCurrentProfile.Name = "radio_OverwriteCurrentProfile";
+            this.radio_OverwriteCurrentProfile.Size = new System.Drawing.Size(156, 19);
+            this.radio_OverwriteCurrentProfile.TabIndex = 0;
+            this.radio_OverwriteCurrentProfile.TabStop = true;
+            this.radio_OverwriteCurrentProfile.Text = "Overwrite Current Profile";
+            this.radio_OverwriteCurrentProfile.UseVisualStyleBackColor = true;
+            this.radio_OverwriteCurrentProfile.CheckedChanged += new System.EventHandler(this.radio_OverwriteExisting_CheckedChanged);
+            // 
+            // radio_SaveAsNewProfile
+            // 
+            this.radio_SaveAsNewProfile.AutoSize = true;
+            this.radio_SaveAsNewProfile.Location = new System.Drawing.Point(26, 52);
+            this.radio_SaveAsNewProfile.Name = "radio_SaveAsNewProfile";
+            this.radio_SaveAsNewProfile.Size = new System.Drawing.Size(129, 19);
+            this.radio_SaveAsNewProfile.TabIndex = 1;
+            this.radio_SaveAsNewProfile.TabStop = true;
+            this.radio_SaveAsNewProfile.Text = "Save As New Profile";
+            this.radio_SaveAsNewProfile.UseVisualStyleBackColor = true;
+            this.radio_SaveAsNewProfile.CheckedChanged += new System.EventHandler(this.radio_SaveAsNewProfile_CheckedChanged);
+            // 
+            // textBox_SaveAsNewProfile
+            // 
+            this.textBox_SaveAsNewProfile.Enabled = false;
+            this.textBox_SaveAsNewProfile.Location = new System.Drawing.Point(26, 77);
+            this.textBox_SaveAsNewProfile.Name = "textBox_SaveAsNewProfile";
+            this.textBox_SaveAsNewProfile.Size = new System.Drawing.Size(138, 23);
+            this.textBox_SaveAsNewProfile.TabIndex = 2;
+            // 
+            // button_SaveProfile
+            // 
+            this.button_SaveProfile.Enabled = false;
+            this.button_SaveProfile.Location = new System.Drawing.Point(195, 104);
+            this.button_SaveProfile.Name = "button_SaveProfile";
+            this.button_SaveProfile.Size = new System.Drawing.Size(75, 23);
+            this.button_SaveProfile.TabIndex = 3;
+            this.button_SaveProfile.Text = "Save";
+            this.button_SaveProfile.UseVisualStyleBackColor = true;
+            this.button_SaveProfile.Click += new System.EventHandler(this.button_SaveProfile_Click);
+            // 
+            // button_CancelProfile
+            // 
+            this.button_CancelProfile.Location = new System.Drawing.Point(287, 104);
+            this.button_CancelProfile.Name = "button_CancelProfile";
+            this.button_CancelProfile.Size = new System.Drawing.Size(75, 23);
+            this.button_CancelProfile.TabIndex = 4;
+            this.button_CancelProfile.Text = "Cancel";
+            this.button_CancelProfile.UseVisualStyleBackColor = true;
+            this.button_CancelProfile.Click += new System.EventHandler(this.button_CancelProfile_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 139);
+            this.Controls.Add(this.button_CancelProfile);
+            this.Controls.Add(this.button_SaveProfile);
+            this.Controls.Add(this.textBox_SaveAsNewProfile);
+            this.Controls.Add(this.radio_SaveAsNewProfile);
+            this.Controls.Add(this.radio_OverwriteCurrentProfile);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.RadioButton radio_OverwriteCurrentProfile;
+    private System.Windows.Forms.RadioButton radio_SaveAsNewProfile;
+    private System.Windows.Forms.TextBox textBox_SaveAsNewProfile;
+    private System.Windows.Forms.Button button_SaveProfile;
+    private System.Windows.Forms.Button button_CancelProfile;
+}
+}
---
+++ TbManagementTool/Form2.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace DataApp
+{
+    public partial class Form2 : Form
+    {
+        string currentProfile;
+        IEnumerable<Control> comboBoxList;
+        string currentBarcodeDelimiter;
+        string clientName;
+        public string newProfile;
+
+        public Form2(string currentProfile, IEnumerable<Control> comboBoxList, string currentBarcodeDelimiter, string clientName)
+        {
+            InitializeComponent();
+
+            this.currentProfile= currentProfile;
+            this.comboBoxList= comboBoxList;
+            this.currentBarcodeDelimiter= currentBarcodeDelimiter;
+            this.clientName = clientName;
+            this.newProfile = "";
+
+        }
+
+        private void button_SaveProfile_Click(object sender, EventArgs e)
+        {
+            if (this.radio_OverwriteCurrentProfile.Checked) overwriteCurrentProfile();
+            if (this.radio_SaveAsNewProfile.Checked) saveNewProfile();
+        }
+        private void radio_OverwriteExisting_CheckedChanged(object sender, EventArgs e)
+        {
+            this.textBox_SaveAsNewProfile.Enabled = false;
+            this.button_SaveProfile.Enabled = true;
+
+        }
+        private void radio_SaveAsNewProfile_CheckedChanged(object sender, EventArgs e)
+        {
+            this.textBox_SaveAsNewProfile.Enabled = true;
+            this.button_SaveProfile.Enabled = true;
+
+        }
+        private void button_CancelProfile_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void overwriteCurrentProfile()
+        {
+            //overwrite currently active profile
+            if (currentProfile.Equals("") || currentProfile == null)
+            {
+                MessageBox.Show("New profile name empty!");
+                return;
+            }
+            //delete current file
+            File.Delete("Profiles\\" + this.clientName + "\\" + this.currentProfile + ".xml");
+
+            //save new
+            if (buildXML(this.currentProfile))
+            {
+                closeForm(currentProfile);
+                
+            }
+        }
+
+        private void saveNewProfile()
+        {
+            //get name of new profile
+            string newProfileName = textBox_SaveAsNewProfile.Text;
+
+            //check if its empty
+            if(newProfileName.Equals(""))
+            {
+                MessageBox.Show("New profile name empty!");
+                return;
+            }
+            //check if it already exists
+            if (checkIfProfileExists(newProfileName))
+            {
+                MessageBox.Show("Profile name already exists for client!");
+                return;
+            }
+
+            /*
+            foreach (System.Windows.Forms.ComboBox comboBox in comboBoxLst)
+            {
+                comboBoxList.Add(new KeyValuePair<string, System.Windows.Forms.ComboBox>(comboBox.Name, comboBox));
+            }              
+            */
+
+            //build xml
+            if (buildXML(newProfileName))
+            {
+                closeForm(newProfileName);
+            }
+
+        }
+
+        private void closeForm(string sendBack) 
+        {
+            this.newProfile = sendBack;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private bool checkIfProfileExists(string toCheck)
+        {
+
+            DirectoryInfo di = new DirectoryInfo("Profiles\\" + this.clientName + "\\");
+            FileInfo[] files = di.GetFiles("*.xml");
+
+            foreach (FileInfo file in files)
+            {
+                string filename = Path.GetFileNameWithoutExtension(file.Name);
+                if (filename.Equals(toCheck)) return true;
+            }
+
+            return false;
+
+
+        }
+        
+        //TOTO
+        private bool buildXML(string newProfileName)
+        {
+            List<XElement> columns = new List<XElement>();
+
+
+            XElement xprofile = new XElement("profile");
+
+            foreach (System.Windows.Forms.ComboBox comboBox in comboBoxList)
+            {
+                //skip unwanted fields
+                string comboBoxName = comboBox.Name.ToString();
+                if (comboBoxName.Equals("profileBox")) continue;
+                if (comboBoxName.Equals("comboBox_DataMapper_RemoveDuplicate")) continue;
+                if (comboBoxName.Equals("comboBox_DataMapper_ClientName")) continue;
+                if (comboBox.Text.Equals("")) continue;
+                xprofile.Add(new XElement("column", new XAttribute("name", comboBox.Name.Substring("comboBox_DataMapper_".Length)), new XAttribute("value", comboBox.Text.ToString())));
+            }
+
+            //barcode
+            xprofile.Add(new XElement("column", new XAttribute("name", "BarcodeDelimiter"), new XAttribute("value", this.currentBarcodeDelimiter)));
+
+            XDocument xmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", null));
+            xmlDoc.Add(xprofile);
+            xmlDoc.Save("Profiles\\" + this.clientName + "\\" + newProfileName + ".xml");
+
+            //reset clientname to fire event
+            /*
+            foreach (System.Windows.Forms.ComboBox comboBox in comboBoxList)
+            {
+                string comboBoxName = comboBox.Name.ToString();
+                if (!comboBoxName.Equals("comboBox_DataMapper_ClientName")) continue;
+                comboBox.Text = this.clientName;
+                
+            }
+            */
+            return true;
+ 
+        }
+
+    }
+}
